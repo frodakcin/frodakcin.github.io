58c6b8d1cbd43da4cec9b6585db26b26bbc28639
Siyong Huang <huangsy222@gmail.com>
2025-09-13 17:09:11 -0400

    First commit

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="m-2">
+        <h1 className="m-auto font-bold text-3xl text-center p-8">
+          Siyong Huang
+        </h1>
+        <p className="text-justify m-auto max-w-[640px]">
+          Welcome to my website. It has not been completed yet.
+        </p>
+      </div>
+    </div>
+  );
+}
